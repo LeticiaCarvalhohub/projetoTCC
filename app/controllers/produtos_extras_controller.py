1532cf3ef9992c34416c85acf3fe33bb3f522b67
Leticia <leticiaacarvalhoo87@gmail.com>
2025-09-11 09:01:14 -0300

    mudanças no cadastro

--- app/controllers/produtos_extras_controller.py
+++ app/controllers/produtos_extras_controller.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, jsonify
 from app.models.produtos_extras import get_produto_extra, insert_produtos_extras, update_produtos_extras, delete_produtos_extras
 
+
 produtos_extras_bp = Blueprint("/api/produtos_extras", __name__)
 
 @produtos_extras_bp.route("/api/produtos_extras")
@@ -10,14 +11,26 @@ def listar_produtos_extra():
 
 @produtos_extras_bp.route("/api/produtos_extras", methods=["POST"])
 def adicionar_produtos_extras():
-    dado = request.get_json()
-    insert_produtos_extras(dado["nome"], dado["categoria"], dado["marca"], dado["cor"],dado["tamanho"], dado["material"], dado["preco_metro"], dado["unidade_medida"], dado["data_cadastro"])
+    dados = request.get_json()
+
+    nome = dados.get("nome")
+    categoria = dados.get("categoria")
+    marca = dados.get("marca")
+    cor = dados.get("cor")
+    tamanho = dados.get("tamanho")
+    material = dados.get("material")
+    preco_base = dados.get("preco_base")
+    unidade_medida = dados.get("unidade_medida")
+    data_cadastro = dados.get("data_cadastro")
+        
+
+    insert_produtos_extras(nome,categoria,marca,cor,tamanho,material,preco_base,unidade_medida,data_cadastro)
     return jsonify({"mensagem": "Produto adicionado com sucesso!"})
 
 @produtos_extras_bp.route("/api/produtos_extras/<codigo_extra>", methods=["PUT"])
 def atualizar_produtos_extras(codigo_extra):
-    dado = request.get_json()
-    update_produtos_extras(codigo_extra, dado["nome"], dado["categoria"], dado["marca"], dado["cor"], dado["tamanho"],dado["material"], dado["preco_metro"], dado["unidade_medida"], dado["data_cadastro"])
+    dados = request.get_json()
+    update_produtos_extras(codigo_extra, dados)
     return jsonify({"mensagem": "Produto atualizado com sucesso!"})
 
 @produtos_extras_bp.route("/api/produtos_extras/<codigo_extra>", methods=["DELETE"])
--- app/controllers/tecido_controller.py
+++ app/controllers/tecido_controller.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, jsonify
 from app.models.tecido import get_tecido, insert_tecidos, update_tecido, delete_tecidos
 
+
 tecido_bp = Blueprint("/api/tecido", __name__)
 
 @tecido_bp.route("/api/tecido")
@@ -10,14 +11,26 @@ def listar_tecido():
 
 @tecido_bp.route("/api/tecido", methods=["POST"])
 def adicionar_tecido():
-    dado = request.get_json()
-    insert_tecidos(dado["nome"], dado["marca"], dado["tipo_tecido"], dado["estampa"], dado["cor"], dado["largura_cm"], dado["preco_metro"], dado["composicao"], dado["peso_g_m2"], dado["data_cadastro"])
+    dados = request.get_json()
+    
+    nome = dados.get("nome")
+    marca = dados.get("marca")
+    tipo_tecido = dados.get("tipo_tecido")
+    estampa = dados.get("estampa")
+    cor = dados.get("cor")
+    largura_cm = dados.get("largura_cm")
+    preco_metro = dados.get("preco_metro")
+    composicao = dados.get("composicao")
+    peso_g_m2 = dados.get("peso_g_m2")
+    data_cadastro = dados.get("data_cadastro")
+
+    insert_tecidos(nome,marca,tipo_tecido,estampa,cor,largura_cm,preco_metro,composicao,peso_g_m2,data_cadastro)
     return jsonify({"mensagem": "Tecido adicionado com sucesso!"})
 
 @tecido_bp.route("/api/tecido/<codigo_tecido>", methods=["PUT"])
 def atualizar_tecido(codigo_tecido):
-    dado = request.get_json()
-    update_tecido(codigo_tecido, dado["nome"], dado["marca"], dado["tipo_tecido"], dado["estampa"],dado["cor"], dado["largura_cm"], dado["preco_metro"], dado["composicao"], dado["peso_g_m2"], dado["data_cadastro"])
+    dados = request.get_json()
+    update_tecido(codigo_tecido, dados)
     return jsonify({"mensagem": "Tecido atualizado com sucesso!"})
 
 @tecido_bp.route("/api/tecido/<codigo_tecido>", methods=["DELETE"])
